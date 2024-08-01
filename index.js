d1d3eaea933c5fb5c3f9e0d6168e8e896ef21d05
AleksaYO <yura.aleksa.98@gmail.com>
2024-08-02 01:32:17 +0300

    finish mobile header

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerMenu = document.querySelector(".hamburger_menu");
 const hamburgerList = document.querySelector(".hamburger_list");
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");
   hamburgerMenu.classList.toggle("is-active");
@@ -14,3 +15,39 @@ hamburgerMenu.addEventListener("click", ({ target }) => {
     target.classList.toggle("is-active");
   }
 });
+
+let slideIndex = 0;
+let indicatorIndex = 0;
+
+const showSlide = (index) => {
+  const slides = document.querySelector(".slides");
+  const totalSlides = document.querySelectorAll(".slide").length;
+  const indicators = document.querySelectorAll(".indicator");
+
+  if (index >= totalSlides) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = totalSlides - 1;
+  } else {
+    slideIndex = index;
+  }
+
+  indicators.forEach((ind) => {
+    const indicatorIndex = parseInt(ind.dataset.num, 10);
+    indicatorIndex - 1 === slideIndex
+      ? ind.classList.add("is-active")
+      : ind.classList.remove("is-active");
+  });
+
+  slides.style.transform = `translateX(${-slideIndex * 100}%)`;
+};
+
+showSlide(slideIndex);
+
+const changeSlide = (n) => {
+  showSlide(slideIndex + n);
+};
+
+setInterval(() => {
+  changeSlide(1);
+}, 3000);
